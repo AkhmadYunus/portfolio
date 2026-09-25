55567de02c015a92ea35a432579780c7ebc50b24
Akhmad Yunus <akmad.yunus@gmail.com>
2026-09-25 11:19:30 +0700

    Fix theme dark

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const darkModeBtn = document.getElementById('darkModeBtn');
+    /* CEK SAAT HALAMAN DIBUKA */
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
+        darkModeBtn.textContent = "☀️";
+    }
+
 darkModeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
-
+    /* CEK SAAT TOMBOL DIKLIK */
     if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
         darkModeBtn.textContent = '☀️';
     } else {
+        localStorage.setItem('theme', 'light');
         darkModeBtn.textContent = '🌙';
     }
 });
